14378b50b61d7cfe360b44b9a53755d8050434d6
Caleb Webber <caleb@codingthemsoftly.com>
2023-12-11 23:49:12 -0500

    update day5 bench

--- bench/day5_bench.exs
+++ bench/day5_bench.exs
@@ -1,8 +1,8 @@
 {_, content} = File.read("./input/2023_5.txt")
-input = content |> String.split("\n") |> Mix.Tasks.Day5.parse()
+
 Benchee.run(
   %{
-    "day5_part2" => fn -> input |> Mix.Tasks.Day5.part2() end
+    "day5_part2" => fn -> content |> String.split("\n") |> Mix.Tasks.Day5.parse() |> Mix.Tasks.Day5.part2() end
   }
 )
 
