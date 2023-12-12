9217bf68f01eac0790a52e0428b7801c303a33bf
Caleb Webber <caleb@codingthemsoftly.com>
2023-12-11 23:52:16 -0500

    remove empty test file

--- test/aoc_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule Day1Test do
-  use ExUnit.Case
-
-  test "solve" do
-    {_, content} = File.read("./test/test1.txt")
-    IO.puts(Day1_2024.solve(content |> String.split("\n")))
-    assert 1 == 1
-  end
-end
